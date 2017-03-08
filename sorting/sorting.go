7342ceae5cfa39524f051e9824a864d282d80dc1
zainulbahar <pehkulon@gmail.com>
2017-03-08 08:24:42 +0700

    +sorting Finish(Zainul Bahar)

--- sorting/sorting.go
+++ sorting/sorting.go
@@ -4,13 +4,29 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 	"strconv"
 	"strings"
 )
 
-func cetak(a []int) int {
-
+func cetak(isi []int, atas int) {
+	for i := atas; i > 0; i-- {
+		for _, k := range isi {
+			if i <= k {
+				fmt.Print("| ")
+			} else {
+				fmt.Print("  ")
+			}
+		}
+		fmt.Println("")
+	}
+	for _, i := range isi {
+		fmt.Print(i)
+		fmt.Print(" ")
+	}
+	fmt.Println("")
 }
+
 func main() {
 	var data []int
 	tinggi := 0
@@ -26,25 +42,25 @@ func main() {
 		}
 		data = append(data, j)
 	}
-	//fmt.Println(data)
+	// fmt.Println(data)
 	for _, i := range data {
 		if tinggi < i {
 			tinggi = i
 		}
 	}
-	fmt.Println(tinggi)
-	for i := tinggi; i > 0; i-- {
-		for _, k := range data {
-			if i <= k {
-				fmt.Print("| ")
-			} else {
-				fmt.Print("  ")
-			}
-		}
-		fmt.Println("")
-	}
-	for _, i := range data {
-		fmt.Print(i)
-		fmt.Print(" ")
-	}
+	// fmt.Println(tinggi)
+	cetak(data, tinggi)
+	// for i, j := range data {
+	// 	for index := 0; index < i+1; index++ {
+	// 		if j < data[index] {
+	// 			j = data[index]
+	// 			data[index] = data[i]
+	// 			data[i] = j
+	// 		}
+	// 	}
+	// }
+	sort.Sort(sort.IntSlice(data))
+	cetak(data, tinggi)
+	sort.Sort(sort.Reverse(sort.IntSlice(data)))
+	cetak(data, tinggi)
 }
